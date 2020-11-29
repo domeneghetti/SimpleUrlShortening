1b3c6c41362f24683206b64ddc5ac4a50e2b4bbd
Fernando Miranda Domeneghetti <fernandomirandadomeneghetti@hotmail.com>
2020-11-29 14:59:05 -0300

    inclusão do front e mudaná de connection sql para mysql

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 import {Sequelize} from 'sequelize';
 
-const sequelize = new Sequelize('mssql://sa:xxxx@localhost:1433/SimpleUrlShortening');
+const sequelize = new Sequelize('mysql://sa:Teste$123@localhost:3306/SimpleUrlShortening');
 
 export default sequelize;
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Logo, HeaderContainer} from './styles';
+
+import Icone from '../../assets/logo.png';
+
+function Header(props) {
+    return (
+        <>
+           <HeaderContainer>
+               <Logo src={Icone} alt='SimpleUrlShortening' />
+               <h1>Simple Url Shortening</h1>
+               <p>{props.children}</p>
+           </HeaderContainer>
+        </>
+    )
+}
+
+export default Header;
