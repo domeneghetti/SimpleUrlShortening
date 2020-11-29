5f7265040291558b3878884e2d9f2d2573a10cd4
Fernando Miranda Domeneghetti <fernandomirandadomeneghetti@hotmail.com>
2020-11-29 15:19:35 -0300

    alteração usuario sa para root

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 import {Sequelize} from 'sequelize';
 
-const sequelize = new Sequelize('mysql://sa:Teste$123@localhost:3306/SimpleUrlShortening');
+const sequelize = new Sequelize('mysql://root:Teste$123@localhost:3306/SimpleUrlShortening');
 
 export default sequelize;
